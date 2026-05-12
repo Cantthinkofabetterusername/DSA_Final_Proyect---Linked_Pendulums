80e598333e719d0a19a60b340bdf25f4d249c617
Cantthinkofabetterusername <pp3drolinar3s@gmail.com>
2026-05-12 11:05:10 -0600

    basic interaction implementation

--- graphics_functions.py
+++ graphics_functions.py
@@ -12,12 +12,20 @@ def generateCircle(center_x, center_y, segments, radius):
     glColor3f(1, 1, 1)
     glEnd()
 
-def generateButon(center_x, center_y, height, width, R, G, B):
-    glBegin(GL_TRIANGLE_STRIP)
+def generateButton(center_x, center_y, height, width, R, G, B):
+    glBegin(GL_TRIANGLE_FAN)
     glColor3f(R, G, B)
     glVertex2f(center_x - width / 2, center_y + height / 2)
     glVertex2f(center_x + width / 2, center_y + height / 2)
     glVertex2f(center_x + width / 2, center_y - height / 2)
     glVertex2f(center_x - width / 2, center_y - height / 2)
+    glColor3f(1, 1, 1)
+    glEnd()
+
+def generateLine(previous_x, previous_y, current_x, current_y):
+    glBegin(GL_LINES)
+    glColor3f(0.7, 0, 0)
+    glVertex2f(previous_x, previous_y)
+    glVertex2f(current_x, current_y)
     glColor3f(1, 1, 1)
     glEnd()
--- main.py
+++ main.py
@@ -9,14 +9,43 @@ def main():
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
     glFrustum(-0.0128, 0.0128, -0.0083, 0.0083, 0.02, 1000)
     glTranslatef(0, 0, -5)
+    camera_pos_x = 0
+    camera_pos_y = 0
+    saved_states = LinkedList()
+    once_pressed = False
+    button_count = 0
     while True:
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    x, y = event.pos
+                    print(f"Clic izquierdo en: {x}, {y}")
+                    if 56 <= x <= 323 and 37 <= y <= 168:
+                        if once_pressed:
+                            button_count += 1
+                        once_pressed = True
+                        saved_states.insert([camera_pos_x, camera_pos_y])
+                        camera_pos_x += 2
         glClearColor(0.2, 0.3, 0.3, 1.0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        generateCircle(0, 0, 360, 0.2)
+        generateCircle(camera_pos_x - camera_pos_x, camera_pos_y - camera_pos_y, 360, 0.2)
+        current_node = saved_states.head
+        if once_pressed == True:
+            generateCircle(saved_states.head.node_data[0] - camera_pos_x, saved_states.head.node_data[1] - camera_pos_y, 360, 0.2)
+            if button_count == 0:
+                generateLine(saved_states.head.node_data[0] - camera_pos_x, saved_states.head.node_data[1] - camera_pos_y, camera_pos_x - camera_pos_x, camera_pos_y - camera_pos_y)
+            else:
+                generateLine(saved_states.head.node_data[0] - camera_pos_x, saved_states.head.node_data[1] - camera_pos_y, saved_states.head.next.node_data[0] - camera_pos_x, saved_states.head.next.node_data[1] - camera_pos_y)
+            for i in range(button_count):
+                generateCircle(current_node.next.node_data[0] - camera_pos_x, current_node.next.node_data[1] - camera_pos_y, 360, 0.2)
+                generateLine(current_node.node_data[0] - camera_pos_x, current_node.node_data[1] - camera_pos_y, 0, 0)
+                current_node = current_node.next
+        generateButton(camera_pos_x - 2.5 - camera_pos_x, camera_pos_y + 1.7, 0.5 - camera_pos_y, 1, 0, 0, 0.8)
+        generateButton(camera_pos_x - 2.5 - camera_pos_x, camera_pos_y + 1.1 - camera_pos_y, 0.5, 1, 0, 0.8, 0.8)
+        generateButton(camera_pos_x - 2.5 - camera_pos_x, camera_pos_y + 0.5 - camera_pos_y, 0.5, 1, 0, 0.8, 0)
         pygame.display.flip()
         pygame.time.wait(10)
 
